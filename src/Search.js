85a07f7e9e657b8209b1acd32c777eba4a4452d6
MarieJosee13 <mariejosee1313@gmail.com>
2023-05-09 15:14:55 -0700

    added axios, added a Result Component
    Added API & API key in my results component for my next step. Made my search engine functional.

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,27 @@
+import React, { useState } from "react";
 import "./Search.css";
-
+import Results from "./Results";
 export default function Search() {
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+  }
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
   return (
     <div className="Search">
-      <form>
-        <input type="text" className="gif-search"></input>
+      <form onSubmit={search}>
+        <input
+          type="search"
+          onChange={handleKeywordChange}
+          className="gif-search"
+        />
         <button type="submit">Go!</button>
       </form>
+      <Results />
     </div>
   );
 }
