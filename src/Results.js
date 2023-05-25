5292d76f2b54c60ae8597b3dffb2adb5d7bd0d03
MarieJosee13 <mariejosee1313@gmail.com>
2023-05-24 17:01:19 -0700

    fixed a few bugs to import the API

--- src/Results.js
+++ src/Results.js
@@ -1,9 +1,10 @@
 import "./Results.css";
 
-let apiKey = "1VktSjFyLglS8YtVcp0hZfEK42UrwyQf";
-let word = "hello";
-let apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${word}&limit=25&offset=0&rating=g&lang=en`;
 export default function Results() {
+  let apiKey = "1VktSjFyLglS8YtVcp0hZfEK42UrwyQf";
+  let word = "hello";
+  let apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${word}&limit=25&offset=0&rating=g&lang=en`;
+
   console.log(apiUrl);
   return " Hello from results";
 }
