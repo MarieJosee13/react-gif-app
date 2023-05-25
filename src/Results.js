0c18eb0c98a79739a514dd6b5e32f6e75d8304f4
MarieJosee13 <mariejosee1313@gmail.com>
2023-05-24 17:22:58 -0700

    called axios to fetch the data from my url

--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,16 @@
+import axios from "axios";
 import "./Results.css";
 
 export default function Results() {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   let apiKey = "1VktSjFyLglS8YtVcp0hZfEK42UrwyQf";
   let word = "hello";
   let apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${word}&limit=25&offset=0&rating=g&lang=en`;
+  axios.get(apiUrl).then(handleResponse);
 
-  console.log(apiUrl);
   return " Hello from results";
 }
 Results();
